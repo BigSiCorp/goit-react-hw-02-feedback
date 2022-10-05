bd3072e9edc2a8b4e328b6f8074cebb32fb72fee
BigSiCorp <si.kocorp@gmail.com>
2022-10-05 21:22:43 +0300

    feedback__init

---
+++ src/components/App.jsx
@@ -0,0 +1,51 @@
+import { Component } from 'react';
+// import { User } from "./User/User";
+import { Feedback } from "./Feedback";
+// import { users } from "data/users"
+// import { Section } from "./Section/Section";
+// import { Button } from './Button';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  // clickHandler = () => {
+  //   this.setState({ isListShown: true });
+  // };
+
+  // userDelete = (userId) => {
+  //   this.setState(prevState => ({
+  //     users: prevState.users.filter(user => user.id !== userId)
+  //   }))
+  // }
+
+  // changeJobStatus = (userId) => {
+  //   this.setState(prevState => ({
+  //     users: prevState.users.map(user => {
+
+  //       if (user.id === userId) {
+  //         return {
+  //           ...user,
+  //           hasjob: !user.hasjob,
+  //         }
+  //       }
+  //       return user
+
+  //     }),
+  //   }));
+  // }
+
+  render() {
+    
+    return (
+      <>
+        
+        <Feedback options={ this.state } />
+        
+      </>
+    );
+  }
+}
---
+++ src/components/Feedback.jsx
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+//import { Paragraf, Span } from './User.styled';
+
+export const Feedback = ({ options: { good, neutral, bad } }) => {
+   
+
+    return (
+        <>
+            <h2>Please leave feedback</h2>
+            <button>Good</button>
+            <button>Neutral</button>
+            <button>Bad</button>
+            <h2>Statistics</h2>
+            <ul>
+                <li>Good: {good}</li>
+                <li>Neutral: {neutral}</li>
+                <li>Bad: {bad}</li>
+            </ul>
+
+        </>
+    );
+}
+
+// User.propTypes = {
+//     user: PropTypes.shape({
+//         name: PropTypes.string.isRequired,
+//         email: PropTypes.string.isRequired,
+//     }).isRequired
+// }
